21db4743b4e3a4942a0070881cf0e480e6145243
karan <114896722+karan4533@users.noreply.github.com>
2024-10-17 19:47:33 +0530

    space

--- Sts/fabanocci.java
+++ Sts/fabanocci.java
@@ -3,7 +3,7 @@ public class fabanocci {
   public static void main(String[]args){
 
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter a number: ");
+    System.out.print("Enter a number : ");
     int number = scanner.nextInt();
 
      int a = 0;
